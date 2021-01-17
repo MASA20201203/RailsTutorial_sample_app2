756f14cfe2366d7bafd04e9589a3026f6f5f5af0
masa <koedame032@gmail.com>
2021-01-17 13:11:05 +0900

    11.3.3 有効化のテストとリファクタリング

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,6 @@ class UsersController < ApplicationController
   before_action :correct_user,   only: %i[edit update]
   before_action :admin_user,     only: :destroy
 
-  def show
-    @user = User.find(params[:id])
-  end
-
   def new
     @user = User.new
   end
@@ -14,7 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      UserMailer.account_activation(@user).deliver_now
+      @user.send_activation_email
       flash[:info] = 'Please check your email to activate your account.'
       redirect_to root_url
     else
@@ -37,7 +33,12 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.paginate(page: params[:page])
+    @users = User.where(activated: true).paginate(page: params[:page])
+  end
+
+  def show
+    @user = User.find(params[:id])
+    redirect_to root_url and return unless @user.activated?
   end
 
   def destroy
